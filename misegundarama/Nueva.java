b53e16a6aca4359f2a98986fe2cd85871a988465
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-15 14:31:16 +0100

    D-hice un cambio

--- misegundarama/Nueva.java
+++ misegundarama/Nueva.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package misegundarama;
 
 /**
@@ -12,5 +6,6 @@ package misegundarama;
  */
 public class Nueva {
 
+    System.out.println("hola de nuevo :)");
 }
 
